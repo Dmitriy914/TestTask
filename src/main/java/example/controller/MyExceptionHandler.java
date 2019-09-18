6f77d47d83995f3cc333d038e9bec5e2cde3299e
Дмитрий <dimasya914@mail.ru>
2019-09-18 15:23:25 +0700

    Refresh Google Style.

--- src/main/java/example/controller/MyExceptionHandler.java
+++ src/main/java/example/controller/MyExceptionHandler.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class MyExceptionHandler {
     @ExceptionHandler(DuplicateException.class)
-    public ResponseEntity<String> Method(DuplicateException e){
+    public ResponseEntity<String> method(DuplicateException e){
         return new ResponseEntity<>(
                 "Field (" + e.getErrorField() + ") is duplicated",
                 HttpStatus.BAD_REQUEST);
@@ -22,21 +22,21 @@ public class MyExceptionHandler {
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Bad amount")
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public void Exception1(){
+    public void exception1(){
     }
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Insufficient funds in account")
     @ExceptionHandler(BalanceException.class)
-    public void Exception2(){
+    public void exception2(){
     }
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Scale cannot be greater than 2")
     @ExceptionHandler(ScaleException.class)
-    public void Exception3(){
+    public void exception3(){
     }
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Amount must be greater than 0")
     @ExceptionHandler(AmountException.class)
-    public void Exception4(){
+    public void exception4(){
     }
 }
