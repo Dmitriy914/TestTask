e25ff170043a8347bb136e804dab06483a6ed320
Дмитрий <dimasya914@mail.ru>
2019-09-19 11:20:52 +0700

    Refactoring.

---
+++ src/test/java/example/service/ServiceBankTest.java
@@ -0,0 +1,18 @@
+package example.service;
+
+import org.junit.Test;
+
+public class ServiceBankTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void searchAll() {
+    }
+
+    @Test
+    public void search() {
+    }
+}
