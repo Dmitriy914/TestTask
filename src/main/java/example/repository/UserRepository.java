be6d78ed4eeadc4d7da6692ff249db5df2589c06
Дмитрий <dimasya914@mail.ru>
2019-09-22 21:04:37 +0700

    Refactoring.

--- src/main/java/example/repository/UserRepository.java
+++ src/main/java/example/repository/UserRepository.java
@@ -2,11 +2,9 @@ package example.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import example.entity.User;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository()
 public interface UserRepository extends CrudRepository<User, Integer> {
     boolean existsByPhone(String phone);
     Optional<User> findByPhone(String phone);
