52aa94ed14f03df5f6a947f46913b1b7d355899d
Дмитрий <dimasya914@mail.ru>
2019-09-24 12:37:16 +0700

    Integration testing BankRepository.

--- src/test/java/example/repository/BankRepositoryIntegrationTest.java
+++ src/test/java/example/repository/BankRepositoryIntegrationTest.java
@@ -1,7 +1,6 @@
 package example.repository;
 
 import example.entity.Bank;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
