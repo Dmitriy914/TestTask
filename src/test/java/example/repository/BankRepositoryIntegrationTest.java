925870256d31d71273305fc001224acb36cec827
Дмитрий <dimasya914@mail.ru>
2019-09-24 12:37:01 +0700

    Integration testing BankRepository.

---
+++ src/test/java/example/repository/BankRepositoryIntegrationTest.java
@@ -0,0 +1,59 @@
+package example.repository;
+
+import example.entity.Bank;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class BankRepositoryIntegrationTest {
+    @Autowired
+    private BankRepository repository;
+
+    private Bank createBank(){
+        Bank bank = new Bank();
+        bank.setName("Name");
+        bank.setAddress("Address");
+        bank.setPhone("Phone");
+        return bank;
+    }
+
+    @Test
+    public void findByName(){
+        Bank bank = createBank();
+        repository.save(bank);
+
+        Bank findBank = repository.findByName(bank.getName()).orElse(null);
+
+        assertNotNull(findBank);
+        assertEquals(bank, findBank);
+    }
+
+    @Test
+    public void findByPhone(){
+        Bank bank = createBank();
+        repository.save(bank);
+
+        Bank findBank = repository.findByPhone(bank.getPhone()).orElse(null);
+
+        assertNotNull(findBank);
+        assertEquals(bank, findBank);
+    }
+
+    @Test
+    public void exists(){
+        Bank bank = createBank();
+        repository.save(bank);
+
+        assertTrue(repository.existsByName(bank.getName()));
+        assertTrue(repository.existsByPhone(bank.getPhone()));
+        assertFalse(repository.existsByPhone("Phone1"));
+        assertFalse(repository.existsByName("Name1"));
+    }
+}
