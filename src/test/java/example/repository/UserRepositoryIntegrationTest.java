abbdd9cc7fe9035cab66e1a3ed9b891d258394fb
Дмитрий <dimasya914@mail.ru>
2019-09-24 10:37:38 +0700

    Integration testing UserRepository.

--- src/test/java/example/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/example/repository/UserRepositoryIntegrationTest.java
@@ -1,32 +1,44 @@
 package example.repository;
 
 import example.entity.User;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserRepositoryIntegrationTest {
     @Autowired
-    private TestEntityManager entityManager;
-
-    @Autowired
     private UserRepository repository;
 
-    @Test
-    public void test(){
+    private User createUser(){
         User user = new User();
         user.setAddress("Address");
         user.setName("Name");
         user.setSurname("Surname");
         user.setPatronymic("Patronymic");
-        user.setPhone("Phones");
-        entityManager.persist(user);
-        entityManager.flush();
+        user.setPhone("Phone");
+        return user;
+    }
+
+    @Test
+    public void findByPhone(){
+        User user = createUser();
+        repository.save(user);
+
+        User saveUser = repository.findByPhone("Phone").orElse(null);
+
+        assertEquals(user, saveUser);
+    }
+
+    @Test
+    public void existsByPhone(){
+        assertFalse(repository.existsByPhone("Phone"));
+        repository.save(createUser());
+        assertTrue(repository.existsByPhone("Phone"));
     }
 }
